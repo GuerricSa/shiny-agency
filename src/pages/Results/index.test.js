5f7ff0a1b4ed9ce047ee0a7362ac38ab9955ea3c
GuerricSa <guerric.sant@gmail.com>
2023-10-30 09:49:46 +0100

    creating test for results

--- src/pages/Results/index.test.js
+++ src/pages/Results/index.test.js
@@ -1,12 +1,24 @@
 import { formatJobList } from './'
+import { formatFetchParams } from './'
 
 describe('Function formatJobList in Results', () => {
-  test('ceci est mon premier test', () => {
+  it("met une virgule après un item qui n'est pas le dernier", () => {
     const expectedState = 'item2,'
     expect(formatJobList('item2', 3, 1)).toEqual(expectedState)
   })
-  test('ne met pas de virgule sur le dernier élément', () => {
+  it('ne met pas de virgule sur le dernier élément', () => {
     const expectedState = 'item3'
     expect(formatJobList('item3', 3, 2)).toEqual(expectedState)
+  })
+})
+
+describe('function formatFetchParams in Results', () => {
+  it('ne met pas de & avant le premier parametre', () => {
+    const expectedState = `a1=test`
+    expect(formatFetchParams({1: "test"})).toEqual(expectedState)
+  })
+  it('met un & avant chaque paramètre sauf le 1er', () => {
+    const expectedState = 'a1=test&a2=test2&a3=test3'
+    expect(formatFetchParams({1: "test", 2: "test2", 3: "test3"})).toEqual(expectedState)
   })
 })
